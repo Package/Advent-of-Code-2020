ee10fc04ad6872080ba68c6198f3b195e92a183b
Package <ryan.dean1000@outlook.com>
2020-12-10 17:02:57 +0000

    Solved Day 2, Part 1.

---
+++ AdventOfCode.Tests/SolutionTests/Test_Solution_Day02.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode.Days;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Tests.SolutionTests
+{
+    [TestClass]
+    public class Test_Solution_Day02
+    {
+
+        [TestMethod]
+        public void Test_Can_Parse_PasswordPolicy()
+        {
+            var policy = PasswordPolicy.Parse("1-3 a: abcde");
+            Assert.AreEqual(policy.Min, 1);
+            Assert.AreEqual(policy.Max, 3);
+            Assert.AreEqual(policy.Character, 'a');
+            Assert.AreEqual(policy.Password, "abcde");
+
+            policy = PasswordPolicy.Parse("1-3 b: cdefg");
+            Assert.AreEqual(policy.Min, 1);
+            Assert.AreEqual(policy.Max, 3);
+            Assert.AreEqual(policy.Character, 'b');
+            Assert.AreEqual(policy.Password, "cdefg");
+
+            policy = PasswordPolicy.Parse("2-9 c: ccccccccc");
+            Assert.AreEqual(policy.Min, 2);
+            Assert.AreEqual(policy.Max, 9);
+            Assert.AreEqual(policy.Character, 'c');
+            Assert.AreEqual(policy.Password, "ccccccccc");
+        }
+
+        [TestMethod]
+        public void Test_Solves_Day01_Part1()
+        {
+            var day = new Day02();
+            var answer = day.First();
+
+            Assert.AreEqual(416, answer);
+        }
+
+        [TestMethod]
+        public void Test_Solves_Day01_Part2()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
---
+++ AdventOfCode/Days/Day02.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Days
+{
+    public class Day02 : Day<int>
+    {
+        public override int First()
+        {
+            var inputs = File.ReadAllLines("Inputs/Day02.txt");
+
+            var policies = new List<PasswordPolicy>();
+            foreach (var line in inputs)
+            {
+                policies.Add(PasswordPolicy.Parse(line));
+            }
+
+            return policies.Count(p => p.IsValid());
+        }
+
+        public override int Second()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PasswordPolicy
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public char Character { get; set; }
+        public string Password { get; set; }
+
+        public static PasswordPolicy Parse(string line)
+        {
+            var policy = new PasswordPolicy();
+
+            var firstSpaceIndex = line.IndexOf(" ");
+
+            var lengths = line.Substring(0, firstSpaceIndex).Split("-");
+            policy.Min = Convert.ToInt32(lengths[0]);
+            policy.Max = Convert.ToInt32(lengths[1]);
+
+            policy.Character = line[firstSpaceIndex + 1];
+
+            policy.Password = line.Substring(line.IndexOf(": ") + 2);
+
+            return policy;
+        }
+
+        public bool IsValid()
+        {
+            var countInPassword = Password.Count(p => p == Character);
+
+            return countInPassword >= Min && countInPassword <= Max;
+        }
+    }
+}
