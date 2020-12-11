acee4eca0ae68d845a5a4c75b95a60a2205a621f
Package <ryan.dean1000@outlook.com>
2020-12-11 17:04:21 +0000

    Solved Day 5, Part 1.

---
+++ AdventOfCode.Tests/SolutionTests/Test_Solution_Day05.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode.Days;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Tests.SolutionTests
+{
+    [TestClass]
+    public class Test_Solution_Day05
+    {
+        [TestMethod]
+        public void Test_Solves_Day04_Part1()
+        {
+            var day = new Day05();
+            var answer = day.First();
+
+            Assert.AreEqual(963, answer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void Test_Solves_Day04_Part2()
+        {
+            var day = new Day05();
+            var answer = day.Second();
+        }
+
+        [TestMethod]
+        public void Test_Can_Parse_BoardingPassCode()
+        {
+            var pass = new BoardingPass("FBFBBFFRLR");
+            Assert.AreEqual(44, pass.Row);
+            Assert.AreEqual(5, pass.Column);
+            Assert.AreEqual(357, pass.Id);
+
+            var pass2 = new BoardingPass("BFFFBBFRRR");
+            Assert.AreEqual(70, pass2.Row);
+            Assert.AreEqual(7, pass2.Column);
+            Assert.AreEqual(567, pass2.Id);
+
+            var pass3 = new BoardingPass("FFFBBBFRRR");
+            Assert.AreEqual(14, pass3.Row);
+            Assert.AreEqual(7, pass3.Column);
+            Assert.AreEqual(119, pass3.Id);
+
+            var pass4 = new BoardingPass("BBFFBBFRLL");
+            Assert.AreEqual(102, pass4.Row);
+            Assert.AreEqual(4, pass4.Column);
+            Assert.AreEqual(820, pass4.Id);
+        }
+    }
+}
---
+++ AdventOfCode/Days/Day05.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Days
+{
+    public class Day05 : Day<int>
+    {
+        public override int First()
+        {
+            var inputs = File.ReadAllLines("Inputs/Day05.txt");
+            return inputs.Select(i => new BoardingPass(i)).Max(i => i.Id);
+        }
+
+        public override int Second()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BoardingPass
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public int Id { get; set; }
+        public string Code { get; set; }
+
+        public BoardingPass(string code)
+        {
+            Code = code;
+
+            Row = ParseCode(Code.Substring(0, 7), 'F', 'B', 0, 127);
+            Column = ParseCode(Code.Substring(7), 'L', 'R', 0, 7);
+
+            Id = (Row * 8) + Column;
+        }
+
+        private int ParseCode(string codes, char lower, char upper, int min, int max)
+        {
+            int minRange = min;
+            int maxRange = max;
+
+            for (var index = 0; index < codes.Length; index++)
+            {
+                var code = codes[index];
+
+                if (index == codes.Length - 1)
+                {
+                    return (code == lower ? minRange : maxRange);
+                }
+                else if (code == lower)
+                {
+                    maxRange = minRange + ((maxRange - minRange) / 2);
+                }
+                else if (code == upper)
+                {
+                    minRange = 1 + (minRange + ((maxRange - minRange) / 2));
+                }
+            }
+
+            return -1;
+        }
+    }
+}
