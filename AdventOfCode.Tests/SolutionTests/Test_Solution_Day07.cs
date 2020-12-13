8b152e9d6085adce070aa4530f47a86ecabffb5b
Package <ryan.dean1000@outlook.com>
2020-12-13 14:32:06 +0000

    Solved Day 07, Part 1.

---
+++ AdventOfCode.Tests/SolutionTests/Test_Solution_Day07.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode.Days;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Tests.SolutionTests
+{
+    [TestClass]
+    public class Test_Solution_Day07
+    {
+        [TestMethod]
+        public void Test_Solves_Day06_Part1()
+        {
+            var day = new Day07();
+            var answer = day.First();
+
+            Assert.AreEqual(268, answer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void Test_Solves_Day06_Part2()
+        {
+            var day = new Day07();
+            var answer = day.Second();
+        }
+
+        [TestMethod]
+        public void Test_Can_Parse_Example_Input()
+        {
+            var day = new Day07();
+            var parsed = day.ParseBagsFromInput("Day07_Example");
+
+            Assert.AreEqual(9, parsed.Count);
+        }
+    }
+}
---
+++ AdventOfCode/Days/Day07.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode.Days
+{
+    public class Day07 : Day<int>
+    {
+        public Dictionary<string, List<BagCount>> Map { get; set; }
+
+        public override int First()
+        {
+            Map = ParseBagsFromInput("Day07");
+
+            var bags = CanHoldBag("shiny gold bag", new HashSet<string>());
+            return bags.Count;
+        }
+
+        public override int Second()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HashSet<string> CanHoldBag(string targetBag, HashSet<string> holdingBags)
+        {
+            var bags = Map.Where(m => m.Value.FirstOrDefault(bc => bc.BagName == targetBag) != null).ToList();
+            foreach (var b in bags)
+            {
+                holdingBags.Add(b.Key);
+                CanHoldBag(b.Key, holdingBags);
+            }
+
+            return holdingBags;
+        }
+
+        public Dictionary<string, List<BagCount>> ParseBagsFromInput(string fileName)
+        {
+            var inputs = File.ReadAllLines($"Inputs/{fileName}.txt")
+                    .Select(l => l.Replace("bags", "bag").Replace(".", ""))
+                    .ToList();
+
+            var bagMap = new Dictionary<string, List<BagCount>>();
+            foreach (var line in inputs)
+            {
+                var split = line.Split(" contain ");
+                var bag = split[0];
+                var otherBags = split[1];
+
+                if (otherBags.Contains("no other bag"))
+                {
+                    bagMap.Add(bag, new List<BagCount>());
+                }
+                else
+                {
+                    var bagList = new List<BagCount>();
+
+                    var splitOtherBags = otherBags.Split(", ");
+                    foreach (var b in splitOtherBags)
+                    {
+                        var firstSpaceIndex = b.IndexOf(" ");
+                        var bagCount = new BagCount
+                        {
+                            BagName = b.Substring(firstSpaceIndex + 1),
+                            NumberOfBags = Convert.ToInt32(b.Substring(0, firstSpaceIndex))
+                        };
+
+                        bagList.Add(bagCount);
+                    }
+
+                    bagMap.Add(bag, bagList);
+                }
+            }
+
+            return bagMap;
+        }
+    }
+
+    public class BagCount
+    {
+        public int NumberOfBags { get; set; }
+        public string BagName { get; set; }
+    }
+}
